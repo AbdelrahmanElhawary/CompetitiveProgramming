85bc47c2828895906418141a18edf062aec1672d
AbdelrahmanElhawary <abdrhmanhassan.ah@gmail.com>
2018-01-30 23:56:59 +0200

    second

---
+++ Hacker/HACKR-connecting-towns.cpp
@@ -0,0 +1,23 @@
+#include cmath
+#include cstdio
+#include vector
+#include iostream
+#include algorithm
+using namespace std;
+
+
+int main() {
+    int t; cint;
+    while(t--)
+    {
+        int n; long long ans=1,mod=1234567;
+        cinn;
+        for(int i=1;in;i++)
+        {
+            int x; cinx;
+            ans=x;ans%=mod;
+        }
+        coutansendl;
+    }
+    return 0;
+}
