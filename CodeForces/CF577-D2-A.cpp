b1fe695f6515b2ac3c0816970587ad188aa9809b
AbdelrahmanElhawary <abdrhmanhassan.ah@gmail.com>
2018-02-26 21:59:16 +0200

    commit

---
+++ CodeForces/CF577-D2-A.cpp
@@ -0,0 +1,40 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include<iostream>
+#include<map>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<set>
+#include<sstream>
+#include<vector>
+#include<string.h>
+#include<queue>
+#include<list>
+#include <iterator>
+#include <functional> 
+#include<iomanip>
+#define F first
+#define S second
+#define ll long long
+#define ld long double
+#define dong long
+using namespace std;
+void fast_in_out() {
+	std::ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL); cout.tie(NULL);
+}
+int ar[26];
+int main()
+{
+	fast_in_out();
+	ll n, m,c=0;
+	cin >> n >> m;
+	for (int i = 1; i <= n;i++) {
+		if (m%i == 0 && m / i <= n) c++;
+	}
+	cout << c << endl;
+	//system("pause");
+	return 0;
+}
